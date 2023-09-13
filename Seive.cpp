8eb30c7f941a9c654946cb5f94acd27883019e85
Abdul Al Mamun <124201619+mamunCSE64@users.noreply.github.com>
2023-09-13 22:48:36 +0600

    Create Seive.cpp

---
+++ Seive.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#define ll long long int
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof pop_front
+#define gcd __gcd
+#define int_string to_string
+#define string_int stoi
+#define mn(v) *min_element(v.begin(), v.end())
+#define mx(v) *max_element(v.begin(), v.end())
+#define index_character s.find('character')
+#define countxchar count(s.begin(), s.end(), 'x')
+#define index_ofX_vector find(v.begin(), v.end(), x) - v.begin()
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define n1 cout << "-1" << endl
+#define sorted is_sorted(v.begin(), v.end())
+#define nl << endl
+#define sp << " "
+#define mp make_pair
+#define fi first
+#define se second
+#define Mx LLONG_MAX
+#define Mn LLONG_MIN
+#define mod %1000000007
+const ll N = 2e6+5;
+// freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+// BesidesDuplicateCharacterEraseInString s.erase(unique(s.begin(), s.end()), s.end());
+// Upper/lower-> transform(s.begin(), s.end(), s.begin(), ::toupper/tolower);
+using namespace std;
+ll i, j, k, n, m;
+const ll e=1e+9;
+bool comp(pair<long double,ll> a,pair<long double,ll> b){
+    if(a.fi==b.fi) return (a.se<b.se); return (a.fi>b.fi);}
+// Don't get stuck on a single approach for long, think of multiple ways
+// You will destroy your dream if you stay in your comfort zone
+// **********************|| Main Code ||********************************
+//
+vector<ll> v(N,1); 
+void fun(){
+    for(int i=2;i*i<=N;i++){
+        if(v[i]){
+            for(int j=i*i;j<=N;j+=i) v[j]=0;
+        }
+    }
+    v[0]=0,v[1]=0;
+}
+bool seive(ll n){
+    if(v[n]) return 1;
+    else return 0;
+}
+int main()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+
+    ll test = 1;
+    cin >> test;   fun();
+    again: 
+    while (test--)
+    {   
+        ll n;
+        cin>>n; 
+        if(seive(n)) cout << "Prime" nl;
+        else cout << "Not Prime" nl;        
+    }       
+}
